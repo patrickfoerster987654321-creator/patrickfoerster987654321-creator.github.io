01a7a265fe7416545124001bf2ecea1b97adcc60
Patrick Foerster <patrickfoerster987654321@gmail.com>
2026-02-12 12:22:27 -0500

    Finished outline and added face outline

--- projects/portrait/script.js
+++ projects/portrait/script.js
@@ -1,3 +1,25 @@
-//declare canvas and ctx elements
+//declare canvas and ctx variables
 const canvas = document.getElementById("me");
 const ctx = canvas.getContext("2d");
+
+//make the face outline
+ctx.fillStyle = "#FFE5B4";
+ctx.beginPath();
+ctx.arc(400,400,100,0,2*Math.PI);
+ctx.fill();
+
+//add mouth
+
+//add nose
+
+//add left eye
+
+//add right eye
+
+//add eyebrows
+
+//add hair
+
+//add neck
+
+
