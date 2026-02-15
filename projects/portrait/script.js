eb663150eef80c2a4e9a94d05c5a51ba1442dbf8
Patrick Foerster <patrickfoerster987654321@gmail.com>
2026-02-15 15:22:11 -0500

    added shoulders

--- projects/portrait/script.js
+++ projects/portrait/script.js
@@ -12,3 +12,20 @@ img.onload = () => {
 //add clothes
 ctx.fillStyle= "#222233";
 ctx.fillRect(260, 480, 280, 220);
+
+//add left shoulder
+ctx.beginPath();
+ctx.moveTo(260, 480);
+ctx.lineTo(200, 620);
+ctx.lineTo(260, 620);
+ctx.closePath();
+ctx.fill();
+
+//add right shoulder
+ctx.beginPath();
+ctx.moveTo(540, 480);
+ctx.lineTo(600, 620);
+ctx.lineTo(540, 620);
+ctx.closePath();
+ctx.fill();
+
