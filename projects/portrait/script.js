a6b80d7525e2f0db7ab74d5959ff901648e3fd27
Patrick Foerster <patrickfoerster987654321@gmail.com>
2026-02-15 15:28:41 -0500

    added collars

--- projects/portrait/script.js
+++ projects/portrait/script.js
@@ -2,12 +2,12 @@
 const canvas = document.getElementById("me");
 const ctx = canvas.getContext("2d");
 
-//add face image
+//make image background
 const img = new Image();
-img.src="me.png";
+img.src = "me.png";
 img.onload = () => {
-	ctx.drawImage(img, 0, 0);
-}
+    ctx.drawImage(img, 0, 0);
+};
 
 //add clothes
 ctx.fillStyle= "#222233";
@@ -32,3 +32,20 @@ ctx.fill();
 //add neck
 ctx.fillStyle = "#f2c29b";
 ctx.fillRect(350, 430, 100, 70);
+
+//add left collar
+ctx.fillStyle = "#1a1a2b";
+ctx.beginPath();
+ctx.moveTo(350, 430);
+ctx.lineTo(320, 470);
+ctx.lineTo(350, 470);
+ctx.closePath();
+ctx.fill();
+
+//add right collar
+ctx.beginPath();
+ctx.moveTo(450, 430);
+ctx.lineTo(480, 470);
+ctx.lineTo(450, 470);
+ctx.closePath();
+ctx.fill();
