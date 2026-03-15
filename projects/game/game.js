2a55c0a0ad16645127f3946da351fae2a8490192
Patrick Foerster <patrickfoerster987654321@gmail.com>
2026-03-15 09:24:52 -0400

    Listed all locations except the secret one

--- projects/game/game.js
+++ projects/game/game.js
@@ -31,12 +31,16 @@ function memorialPlaza() {
     clear();
     print("\nYou are in the Memorial Plaza");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tBroken Bridge");
+        "\n\tBroken Bridge" + "\n\tWoods Trail" + "\n\tSwamp Trail");
     
     function processInput(input){
         if (input.toLowerCase() === "brokenbridge" || "broken bridge") {
             brokenBridge();
-        } else {
+        } else if (input.toLowerCase === "woodstrail" || "woods trail"){
+		woodsTrail();
+	} else if (input.toLowerCase === "swamptrail" || "swamp trail"){
+		swampTrail();
+	} else {
             stayHere();
             waitThenCall(memorialPlaza);
         }
@@ -48,19 +52,76 @@ function restrooms() {
 	clear();
 	print("\nYou are at the Restrooms!");
 	print("\nWhere do you want to go next? Say one of these choices:" +
-	"\n\tBroken Bridge");
-
+	"\n\tBroken Bridge" + "\n\tSwamp Trail");
  	function processInput(input){
  		if (input.toLowerCase() === "brokenbridge" || "broken bridge") {
 			brokenBridge();
-		} else {
+		} else if (input.toLowerCase() === "swamptrail" || "swamp trail"){
+			swampTrail();
+		}else {
 			stayHere();
 			waitThenCall(restrooms);
 		}
 	}
 	waitForInput(processInput);
 }
+function swampTrail() {
+	clear();
+	print("\nYou are at the broken bridge!");
+	print("\nWhere do you want to go next? Say one of these choices:" +
+	"\n\tMemorial Plaza" + "\n\tUpland Trail" + "\n\tRestrooms");
 
+ 	function processInput(input){
+ 		if (input.toLowerCase() === "memorialplaza" || "memorial plaza") {
+			memorialPlaza();
+		} else if (input.toLowerCase() === "restrooms") {
+			restrooms();
+		} else if (input.toLowerCase() === "uplandtrail" || "upland trail"){
+			uplandTrail();
+		}
+			else {      
+			stayHere();
+			waitThenCall(swampTrail);
+		}
+	}
+	waitForInput(processInput);
+}
+function woodsTrail() {
+	clear();
+	print("\nYou are at the broken bridge!");
+	print("\nWhere do you want to go next? Say one of these choices:" +
+	"\n\tMemorial Plaza" + "\n\tUpland Trail");
+
+ 	function processInput(input){
+ 		if (input.toLowerCase() === "memorialplaza" || "memorial plaza") {
+			memorialPlaza();
+		} else if (input.toLowerCase() === "uplandtrail" || "upland trail") {
+			uplandTrail();
+		} else {
+			stayHere();
+			waitThenCall(woodsTrail);
+		}
+	}
+	waitForInput(processInput);
+}
+function uplandTrail() {
+	clear();
+	print("\nYou are at the broken bridge!");
+	print("\nWhere do you want to go next? Say one of these choices:" +
+	"\n\tWoods Trail" + "\n\tSwamp Trail");
+
+ 	function processInput(input){
+ 		if (input.toLowerCase() === "woodstrail" || "woods trail") {
+			woodsTrail();
+		} else if (input.toLowerCase() === "swamptrail" || "swamp trail") {
+			swampTrail();
+		} else {
+			stayHere();
+			waitThenCall(uplandTrail);
+		}
+	}
+	waitForInput(processInput);
+}
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
